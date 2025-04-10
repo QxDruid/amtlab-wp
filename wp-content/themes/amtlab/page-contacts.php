7002fa16c5215b3f083745dd308f1ecb9d893d4e
Egor Savelyev <savelyev.e.s@ya.ru>
2025-04-11 00:08:31 +0700

    added contacts

---
+++ wp-content/themes/amtlab/page-contacts.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Contacts
+ * Description: A template for the contacts page
+ */
+
+get_header(); ?>
+
+<div class="outer" id="contentwrap">
+    <div class="postcont">
+        <div id="content">
+            <h1 class="page-title"><span>Контакты</span></h1>
+            
+            <div class="contact-info">
+                <p>
+                    <strong>Адрес:</strong><br>
+                    634050, Томск, пл. Новособорная, 1<br>
+                    СФТИ ТГУ<br>
+                    Лаборатория новых материалов и перспективных технологий
+                </p>
+                
+                <p>
+                    <strong>Контактная информация:</strong><br>
+                    Тел./Факс.: +7(3822)53-15-91<br>
+                    E-mail: <a href="mailto:amtlab@mail.ru">amtlab@mail.ru</a>
+                </p>
+            </div>
+            
+            <div class="map-container">
+                <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=8gbdYE1oQw83KeiYvfWv8pmSZeFtOehf&width=600&height=450"></script>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?> 
