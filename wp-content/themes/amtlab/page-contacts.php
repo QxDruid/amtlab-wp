38b72dea8602644e058f8de0f6c5d35167424a1e
Egor Savelyev <savelyev.e.s@ya.ru>
2025-04-21 23:24:01 +0700

    added new contacts page

--- wp-content/themes/amtlab/page-contacts.php
+++ wp-content/themes/amtlab/page-contacts.php
@@ -9,25 +9,31 @@ get_header(); ?>
 <div class="outer" id="contentwrap">
     <div class="postcont">
         <div id="content">
-            <h1 class="page-title"><span>Контакты</span></h1>
-            
-            <div class="contact-info">
-                <p>
-                    <strong>Адрес:</strong><br>
-                    634050, Томск, пл. Новособорная, 1<br>
-                    СФТИ ТГУ<br>
-                    Лаборатория новых материалов и перспективных технологий
-                </p>
+            <div class="contact-container">
+                <div class="contact-info">
+                    <div class="contact-block">
+                        <h2>Адрес</h2>
+                        <p>
+                            634050, Томск, пл. Новособорная, 1<br>
+                            СФТИ ТГУ<br>
+                            Лаборатория новых материалов и перспективных технологий
+                        </p>
+                    </div>
+                    
+                    <div class="contact-block">
+                        <h2>Контактная информация</h2>
+                        <p>
+                            <strong>Тел./Факс:</strong> +7(3822)53-15-91<br>
+                            <strong>E-mail:</strong> <a href="mailto:amtlab@mail.ru">amtlab@mail.ru</a>
+                        </p>
+                    </div>
+                </div>
                 
-                <p>
-                    <strong>Контактная информация:</strong><br>
-                    Тел./Факс.: +7(3822)53-15-91<br>
-                    E-mail: <a href="mailto:amtlab@mail.ru">amtlab@mail.ru</a>
-                </p>
-            </div>
-            
-            <div class="map-container">
-                <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=8gbdYE1oQw83KeiYvfWv8pmSZeFtOehf&width=600&height=450"></script>
+                <div class="map-container">
+                    <div class="map-wrapper">
+                        <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=8gbdYE1oQw83KeiYvfWv8pmSZeFtOehf&width=100%&height=450"></script>
+                    </div>
+                </div>
             </div>
             
         </div>
